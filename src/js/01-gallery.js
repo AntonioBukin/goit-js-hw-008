ba683e189364c0480e304a910b2a26f3ebad086d
AntonioBukin <bav1905@gmail.com>
2023-01-16 15:52:04 +0200

    test

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,7 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+hello = "viva conrgats"
+console.log(hello)
 console.log(galleryItems);
 
